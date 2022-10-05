019ea8abd5488c76594a9fc6e81b937b0b6bcb8a
JI-NAMEUN <skadms1720@gmail.com>
2022-10-05 22:34:25 +0900

    accessToken 설정

--- src/main/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
+++ src/main/java/com/ll/exam/app__2022_10_04/app/jwt/JwtProvider.java
@@ -1,12 +1,18 @@
 package com.ll.exam.app__2022_10_04.app.jwt;
 
-import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.Jwts;
+import com.ll.exam.app__2022_10_04.util.Util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jdk.jshell.execution.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -16,5 +22,15 @@ public class JwtProvider {
 
     private SecretKey getSecretKey() {
         return jwtSecretKey;
+    }
+    public String generateAccessToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Util.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
     }
 }
